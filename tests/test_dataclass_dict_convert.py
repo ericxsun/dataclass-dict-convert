224c21060901086ac5ef21cec842840e988abe90
Wim Van de Meerssche <wim.vandemeerssche@intec.ugent.be>
2021-01-05 10:07:48 +0100

    first version

---
+++ tests/test_dataclass_dict_convert.py
@@ -0,0 +1,410 @@
+import dataclasses
+import json
+from dataclasses import dataclass
+from enum import Enum
+from typing import List, Optional, Union, Dict
+
+import pytest
+from stringcase import camelcase
+
+from dataclass_dict_convert import dataclass_dict_convert, create_wrap_in_list_from_convertor, \
+    create_dict_of_dataclasses_to_convertor, create_dict_of_dataclasses_from_convertor
+from dataclass_dict_convert.convert import _is_optional
+
+
+def test_dataclass_dict_convert_1():
+    @dataclass_dict_convert(
+        dict_letter_case=camelcase
+    )
+    @dataclass(frozen=True)
+    class Test:
+        an_int: int
+        a_str: str
+        a_float: float
+        a_bool: bool
+
+    the_instance = Test(1, 'foo', 0.1, True)
+    the_dict = {
+        'anInt': 1,
+        'aStr': 'foo',
+        'aFloat': 0.1,
+        'aBool': True,
+    }
+
+    assert 'anInt' == camelcase('an_int')
+
+    expected = the_dict
+    actual = the_instance.to_dict()
+    assert actual == expected
+
+    expected = the_instance
+    actual = Test.from_dict(the_dict)
+    assert actual == expected
+
+
+def test_dataclass_dict_convert_2():
+    @dataclass_dict_convert(
+        dict_letter_case=camelcase
+    )
+    @dataclass(frozen=True)
+    class Test:
+        a_list: List[int]
+        b_list: List[int]
+
+    the_instance = Test([1, 2, 3], [])
+    the_dict = {
+        'aList': [1, 2, 3],
+        'bList': [],
+    }
+
+    expected = the_dict
+    actual = the_instance.to_dict()
+    assert actual == expected
+
+    expected = the_instance
+    actual = Test.from_dict(the_dict)
+    assert actual == expected
+
+
+def test_is_optional_1():
+    @dataclass
+    class Test:
+        an_opt1: Optional[int]
+        an_opt2: Union[None, int]
+        an_opt3: Union[int, None]
+        an_opt4: Union[Optional[int], int]
+        an_opt5: Union[List[int], None]
+        an_opt6: Union[None, List[int]]
+
+    for field in dataclasses.fields(Test):
+        assert _is_optional(field.type)
+
+
+def test_is_optional_2():
+    @dataclass
+    class Test:
+        an_opt1: int
+        an_opt2: Union[str, int]
+        an_opt3: Union[List[int], int]
+        an_opt4: Union[List[str], List[int]]
+
+    for field in dataclasses.fields(Test):
+        assert not _is_optional(field.type)
+
+
+def test_dataclass_dict_convert_3():
+    @dataclass_dict_convert(
+        dict_letter_case=camelcase
+    )
+    @dataclass(frozen=True)
+    class Test:
+        an_opt: Optional[int]
+        an_opt2: Optional[int]
+
+    the_instance = Test(42, None)
+    the_dict = {
+        'anOpt': 42,
+        'anOpt2': None,
+    }
+
+    expected = the_dict
+    actual = the_instance.to_dict()
+    assert actual == expected
+
+    expected = the_instance
+    actual = Test.from_dict(the_dict)
+    assert actual == expected
+
+
+def test_dataclass_dict_convert_4():
+    @dataclass_dict_convert(
+        dict_letter_case=camelcase
+    )
+    @dataclass(frozen=True)
+    class TestB:
+        an_int: int
+        a_str: str
+        a_float: float
+        a_bool: bool
+
+    @dataclass_dict_convert(
+        dict_letter_case=camelcase
+    )
+    @dataclass(frozen=True)
+    class Test:
+        nestedClass: TestB
+        nestedInOpt: Optional[TestB]
+        nestedInList: List[TestB]
+
+    the_instanceB1 = TestB(1, 'foo', 0.1, True)
+    the_instanceB2 = TestB(2, 'bar', 0.2, False)
+    the_instanceB3 = TestB(3, 'baz', 0.3, True)
+    the_instanceB4 = TestB(4, 'huh', 0.4, False)
+    the_instance = Test(the_instanceB1, the_instanceB2, [the_instanceB3, the_instanceB4])
+    the_dict = {
+        'nestedClass': {'anInt': 1, 'aStr': 'foo', 'aFloat': 0.1, 'aBool': True,},
+        'nestedInOpt': {'anInt': 2, 'aStr': 'bar', 'aFloat': 0.2, 'aBool': False,},
+        'nestedInList': [
+            {'anInt': 3, 'aStr': 'baz', 'aFloat': 0.3, 'aBool': True, },
+            {'anInt': 4, 'aStr': 'huh', 'aFloat': 0.4, 'aBool': False, },
+        ],
+    }
+
+    expected = the_dict
+    actual = the_instance.to_dict()
+    assert actual == expected
+
+    expected = the_instance
+    actual = Test.from_dict(the_dict)
+    assert actual == expected
+
+
+def test_dataclass_dict_convert_5fail():
+    @dataclass_dict_convert(
+        dict_letter_case=camelcase
+    )
+    @dataclass(frozen=True)
+    class Test:
+        a_list: List[int]
+        b_list: List[str]
+
+    the_instance = Test([1], ['ahah'])
+    in_dict = {
+        'aList': 1,
+        'bList': 'ahah',
+    }
+    out_dict = {
+        'aList': [1],
+        'bList': ['ahah'],
+    }
+
+    expected = out_dict
+    actual = the_instance.to_dict()
+    assert actual == expected
+
+    expected = the_instance
+    with pytest.raises(TypeError):
+        actual = Test.from_dict(in_dict)
+        assert actual == expected
+
+
+def test_dataclass_dict_convert_5():
+    @dataclass_dict_convert(
+        dict_letter_case=camelcase,
+        custom_from_dict_convertors={
+            'a_list': create_wrap_in_list_from_convertor(int),
+            'b_list': create_wrap_in_list_from_convertor(str),
+        }
+    )
+    @dataclass(frozen=True)
+    class Test:
+        a_list: List[int]
+        b_list: List[str]
+
+    the_instance = Test([1], ['ahah'])
+    in_dict = {
+        'aList': 1,
+        'bList': 'ahah',
+    }
+    out_dict = {
+        'aList': [1],
+        'bList': ['ahah'],
+    }
+
+    expected = out_dict
+    actual = the_instance.to_dict()
+    assert actual == expected
+
+    expected = the_instance
+    actual = Test.from_dict(in_dict)
+    assert actual == expected
+
+
+def test_dataclass_dict_convert_6():
+    class MyEnum(Enum):
+        TESTA = 'Test A'
+        TESTB = 'Test B'
+
+    @dataclass_dict_convert(
+        dict_letter_case=camelcase
+    )
+    @dataclass(frozen=True)
+    class Test:
+        an_enum: MyEnum
+        an_opt_enum1: Optional[MyEnum]
+        an_opt_enum2: Optional[MyEnum]
+
+    the_instance = Test(MyEnum.TESTA, MyEnum.TESTB, None)
+    the_dict = {
+        'anEnum': 'TESTA',
+        'anOptEnum1': 'TESTB',
+        'anOptEnum2': None,
+    }
+
+    expected = the_dict
+    actual = the_instance.to_dict()
+    assert actual == expected
+
+    expected = the_instance
+    actual = Test.from_dict(the_dict)
+    assert actual == expected
+
+
+def test_dataclass_dict_convert_none_1():
+    class MyEnum(Enum):
+        TESTA = 'Test A'
+        TESTB = 'Test B'
+
+    @dataclass_dict_convert(
+        dict_letter_case=camelcase
+    )
+    @dataclass(frozen=True)
+    class Test:
+        an_enum: MyEnum
+        an_opt_enum1: Optional[MyEnum]
+        an_opt_enum2: Optional[MyEnum]
+        an_opt_int1: Optional[int]
+        an_opt_int2: Optional[int]
+        an_opt_list1: Optional[List[int]]
+        an_opt_list2: Optional[List[int]]
+
+    the_instance = Test(MyEnum.TESTA, MyEnum.TESTB, None,
+                        0, None, [], None)
+    the_dict = {
+        'anEnum': 'TESTA',
+        'anOptEnum1': 'TESTB',
+        'anOptEnum2': None,
+        'anOptInt1': 0,
+        'anOptInt2': None,
+        'anOptList1': [],
+        'anOptList2': None,
+    }
+    the_dict_no_none = {
+        'anEnum': 'TESTA',
+        'anOptEnum1': 'TESTB',
+        'anOptInt1': 0,
+        'anOptList1': [],
+    }
+
+    expected = the_dict
+    actual = the_instance.to_dict(remove_none=False)
+    assert actual == expected
+
+    expected = the_dict_no_none
+    actual = the_instance.to_dict(remove_none=True)
+    assert actual == expected
+
+    expected = the_instance
+    actual = Test.from_dict(the_dict)
+    assert actual == expected
+
+
+def test_dataclass_dict_convert_multi_1():
+    @dataclass_dict_convert(
+        dict_letter_case=camelcase
+    )
+    @dataclass(frozen=True)
+    class Test:
+        an_int: int
+        a_str: str
+        a_float: float
+        a_bool: bool
+
+    multi_instance = [Test(1, 'foo', 0.1, True), Test(2, 'bar', 0.2, False), Test(3, 'baz', 0.3, True)]
+    multi_dict = [
+        {'anInt': 1, 'aStr': 'foo', 'aFloat': 0.1, 'aBool': True,},
+        {'anInt': 2, 'aStr': 'bar', 'aFloat': 0.2, 'aBool': False,},
+        {'anInt': 3, 'aStr': 'baz', 'aFloat': 0.3, 'aBool': True,},
+    ]
+
+    expected = multi_dict
+    actual = Test.to_dict_list(multi_instance)
+    assert actual == expected
+
+    expected = multi_instance
+    actual = Test.from_dict_list(multi_dict)
+    assert actual == expected
+
+
+def test_dataclass_dict_convert_json_1():
+    @dataclass_dict_convert(
+        dict_letter_case=camelcase
+    )
+    @dataclass(frozen=True)
+    class Test:
+        an_int: int
+        a_str: str
+        a_float: float
+        a_bool: bool
+
+    the_instance = Test(1, 'foo', 0.1, True)
+    the_json = """{ "anInt": 1, "aStr": "foo", "aFloat": 0.1, "aBool": true }"""
+
+    expected = json.loads(the_json)
+    actual = json.loads(the_instance.to_json())
+    assert actual == expected
+
+    expected = the_instance
+    actual = Test.from_json(the_json)
+    assert actual == expected
+
+
+
+def test_dataclass_dict_convert_7():
+    @dataclass_dict_convert(dict_letter_case=camelcase)
+    @dataclass(frozen=True)
+    class TesterB:
+        an_int: int
+
+    @dataclass_dict_convert(
+        dict_letter_case=camelcase,
+        custom_from_dict_convertors={
+            'dict_of_tester_b': create_dict_of_dataclasses_from_convertor(TesterB, 'dict_of_tester_b')
+        },
+        custom_to_dict_convertors={
+            'dict_of_tester_b': create_dict_of_dataclasses_to_convertor('dict_of_tester_b')
+        })
+    @dataclass(frozen=True)
+    class TesterA:
+        dict_of_tester_b: Dict[str, TesterB]
+
+    the_instance = TesterA(dict_of_tester_b={
+        'b1': TesterB(5),
+        'b2': TesterB(6)
+    })
+    the_json = """{ "dictOfTesterB": { "b1": { "anInt": 5 }, "b2": { "anInt": 6 } } }"""
+
+    expected = json.loads(the_json)
+    actual = json.loads(the_instance.to_json())
+    assert actual == expected
+
+    expected = the_instance
+    actual = TesterA.from_json(the_json)
+    assert actual == expected
+
+
+def test_dataclass_dict_convert_nested_dict_8():
+    @dataclass_dict_convert(
+        dict_letter_case=camelcase
+    )
+    @dataclass(frozen=True)
+    class Test:
+        a: Dict
+        b: Optional[Dict]
+        c: Optional[Dict]
+
+    the_instance = Test({'foo': 'bar', 'baz': 1}, {'fooB': 'barB', 'bazB': 2}, None)
+    the_dict = {
+        'a': {'foo': 'bar', 'baz': 1},
+        'b': {'fooB': 'barB', 'bazB': 2},
+        'c': None
+    }
+
+    expected = the_dict
+    actual = the_instance.to_dict()
+    assert actual == expected
+
+    expected = the_instance
+    actual = Test.from_dict(the_dict)
+    assert actual == expected
+
